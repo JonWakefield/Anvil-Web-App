ef0c46847b9d3d5cc3829a4b988330bd996bd88b
JonWakefield <jonwakefield.mi@gmail.com>
2023-11-29 10:31:04 -0600

    modified:   DevOps/Docker/docker-compose.yaml 	modified:   DevOps/Docker/init.sql

--- DevOps/Docker/init.sql
+++ DevOps/Docker/init.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS camera_nodes;
 USE camera_nodes;
 -- ADD OTHER sql COMMANDS to initialize if needed
 
--- create user `ginuser`
-CREATE USER 'ginuser'@'maria-db' IDENTIFIED BY 'Hello2018';
-GRANT ALL PRIVILEGES ON camera_nodes.* TO 'ginuser'@'maria-db';
+-- create user `'cotton_user'`
+CREATE USER 'cotton_user'@'maria-db' IDENTIFIED BY 'db1234';
+GRANT ALL PRIVILEGES ON camera_nodes.* TO 'cotton_user'@'maria-db';
 FLUSH PRIVILEGES;
